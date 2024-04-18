86260f0b3c87b99bc9633382302302dce3a57f6a
simoneaucello <simoneaucello@gmail.com>
2024-04-18 14:43:23 +0200

    [add] addNewSong function working

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ createApp({
     data(){
       return{
         apiUrl: 'server.php',
-        diskList: []
+        diskList: [],
+        newSong:{
+          title: "",
+          author: "",
+          year: null,
+          poster: "",
+          genre: ""
+        }
       }
     },
 
@@ -14,8 +21,47 @@ createApp({
         axios.get(this.apiUrl)
         .then((result) => {
           this.diskList = result.data;
-          console.log(this.diskList);
         });
+      },
+
+      addNewSong(){
+        /*
+        1. strutturo i dati per inviarli in POST  a server.php
+        2. invio con axios in POST i dati
+        3. con i dati che ricevo aggiorno la lista delle songs
+        */
+
+        const data = new FormData();
+        data.append("newSongTitle", this.newSong.title);
+        data.append("newSongAuthor", this.newSong.author);
+        data.append("newSongYear", this.newSong.year);
+        data.append("newSongPoster", this.newSong.poster);
+        data.append("newSongGenre", this.newSong.genre);
+
+        axios.post(this.apiUrl, data)
+        .then(result => {
+          this.diskList = result.data;
+          this.newSong.title = '';
+          this.newSong.author = '';
+          this.newSong.year = '';
+          this.newSong.poster = '';
+          this.newSong.genre = '';
+        })
+      },
+
+      removeSong(index){
+        const songToDelete = this.diskList[index];
+
+        if(confirm(`Sei sicuro di voler eliminare il brano ${songToDelete.title}?`));
+        const data = new FormData();
+        data.append('indexToDelete', index);
+
+        axios.post(this.apiUrl, data)
+        .then(result => {
+          this.diskList = result.data;
+        })
+
+
       }
     },
 
--- server.php
+++ server.php
@@ -3,14 +3,30 @@
 $json_string = file_get_contents('dischi.json');
 
 
-$disk_list = json_decode($json_string);
+$disk_list = json_decode($json_string, true);
 
 
-/*
+// verifico che esista in POST la variabile newSongTitle
+// se esiste aggiungo un nuovo brano alla lista e poi aggiorno il file dischi.json con la lista decodificata in testo
 
-logica di modifica cancellazione dei dati 
+if (isset($_POST['newSongTitle'])) {
+  $new_song = [
+    'title' => $_POST['newSongTitle'],
+    'author' => $_POST['newSongAuthor'],
+    'year' => $_POST['newSongYear'],
+    'poster' => $_POST['newSongPoster'],
+    'genre' => $_POST['newSongGenre'],
+  ];
+  $disk_list[] = $new_song;
+  file_put_contents('dischi.json', json_encode($disk_list));
+}
 
-*/
+// se mi arriva in POST songToDelete elimino il post all'indice e aggiorno i dati
+if (isset($_POST['songToDelete'])) {
+  $indexToDelete = $_POST['indexToDelete'];
+  array_splice($disk_list, $indexToDelete, 1);
+  file_put_contents('dischi.json', json_encode($disk_list));
+}
 
 header('Content-Type: application/json');
 
